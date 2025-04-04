daba72682efc6cf2cf7d784c6fa002c3ab306412
Christof <info@christof-kemper.de>
2025-04-04 08:48:44 +0200

    Projektdateien hinzufügen.

---
+++ Bibliothek-Liste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibliothek_Liste
+{
+    class Buch
+    {
+        public string Titel { get; set; }
+        public string Author { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Bibliothek";
+
+            List<Buch> buecher = new List<Buch>() {
+                new Buch() { Titel = "Vom Winde verweht", Author = "Hans Mueller"},
+                new Buch() { Titel = "Vom Sturm verweht", Author = "Hans Mueller"}
+            };
+
+            do
+            {
+                #region Menue
+                Console.WriteLine(buecher[1].Author);
+
+
+                #endregion Menue
+                Console.ReadLine();
+            } while (true);
+        }
+    }
+}
